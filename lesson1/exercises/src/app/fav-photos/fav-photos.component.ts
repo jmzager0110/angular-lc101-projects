8d228d1eec0080d0502bb93dbb76ebe4079eae27
Jessica Zager <jmzager@gmail.com>
2021-08-11 18:26:17 -0500

    Lesson1 Complete

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Pix So Fav They Are Considered On Fleek';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://preview.redd.it/vu7halmtq1k41.jpg?width=960&crop=smart&auto=webp&s=cdc1f0b75589269cd7e14f30812b57acb56b02ac';
+  image3 = 'https://funnyasduck.net/wp-content/uploads/2013/08/funny-guinea-pig-sun-glasses-animal-deal-with-it-pics.jpg';
 
   constructor() { }
 
